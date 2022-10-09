75bc918daf9b908fbadcaeea4d87b61a7935bc7b
sasha <alexandrpudov7@gmail.com>
2022-10-09 12:37:31 +0300

    Добавьте файлы проекта.

---
+++ n1/Program.cs
@@ -0,0 +1,22 @@
+﻿public class sdelau
+{
+    static double finput()
+    {
+        try
+        {
+            double pr = Convert.ToDouble(Console.ReadLine());
+            return pr;
+        }
+        catch
+        {
+            Console.WriteLine("Ввел хуйню, давай по-новой");
+            return finput();
+        }
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Напиши цифру");
+        double n = finput();
+        Console.WriteLine((n == (int)n) ? "целое" : "нихуя, дробное");
+    }
+}
